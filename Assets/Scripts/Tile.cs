e3ea5449831f068d5d8613ac129f7c642f30671a
amit800IL <108745954+amit800IL@users.noreply.github.com>
2024-01-30 23:19:30 +0200

    random sprite colors

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -6,7 +6,16 @@ public class Tile : MonoBehaviour
     [field: SerializeField] public GameObject TilePrefab { get; private set; }
     [SerializeField] private TileType tileType;
     [SerializeField] private SpriteRenderer spriteRenderer;
-    private List<Vector2Int> positions; 
+    private Vector2Int tilePosition;
+
+    private void Start()
+    {
+        Color[] colors = new Color[5] {Color.white, Color.red, Color.green, Color.cyan, Color.magenta};
+
+        int randomColor = Random.Range(0, colors.Length);
+
+        spriteRenderer.color = colors[randomColor];
+    }
 }
 
 public enum TileType
