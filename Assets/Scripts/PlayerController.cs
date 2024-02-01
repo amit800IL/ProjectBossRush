ba28b78b18b9892f657cba51992e44480fdfaf8c
amit800IL <108745954+amit800IL@users.noreply.github.com>
2024-02-01 18:04:11 +0200

    few changes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class Controller : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     private BossRush inputActions;
     private Vector2 inputPosition;
