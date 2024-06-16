43258a46221060ec117ecd0f7a573a34cb1cb37a
amit800IL <108745954+amit800IL@users.noreply.github.com>
2024-06-16 12:59:28 +0300

    main menu buttons added to win and lose screens

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -8,6 +8,7 @@ public class MainMenu : MonoBehaviour
 {
     public void StartGame()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
