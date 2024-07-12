cf46f7596fd657a8fe26c40a85c79305716e6fda
amit800IL <108745954+amit800IL@users.noreply.github.com>
2024-07-12 11:47:47 +0300

    Revert "a little fix"
    This reverts commit 36d2e991511d4100bee7f4ed9a9c613bf147fa76.

--- Assets/Scripts/Player/Classes/Ranger.cs
+++ Assets/Scripts/Player/Classes/Ranger.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 
 public class Ranger : Hero
 {
-    [SerializeField] private Transform endingPosition;
     protected override void Start()
     {
         SymbolTable = new SymbolTable((int)SymbolTable.Symbols.Ranger);
@@ -16,7 +15,6 @@ public class Ranger : Hero
 
     protected override IEnumerator ActivateAttackVfx(Boss boss)
     {
-        attackVFX.SetVector3("Pos4", boss.transform.position);
         yield return base.ActivateAttackVfx(boss);
     }
 
