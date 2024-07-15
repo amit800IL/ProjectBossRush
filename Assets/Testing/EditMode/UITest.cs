e0c82bc72ec1357fe6fdc53a91aebee99f26eb1d
amit800IL <108745954+amit800IL@users.noreply.github.com>
2024-07-15 20:17:47 +0300

    play mode and edit mode tests added

---
+++ Assets/Testing/EditMode/UITest.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class UITest
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void UITestSimplePasses()
+    {
+       
+    }
+
+
+}
---
+++ Assets/Testing/PlayMode/PlayerControllerTest.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class PlayerControllerTest
+{
+    [UnityTest]
+    public IEnumerator PlayerControllerTestWithEnumeratorPasses()
+    {
+       
+        yield return null;
+    }
+}
