bc66099650b80b99f8e5d4627bd0aa6dffcc1ccd
amit800IL <108745954+amit800IL@users.noreply.github.com>
2024-08-09 17:41:53 +0300

    throwing weapons working correctly

--- Assets/Scripts/Player/HeroThrowingWeapon.cs
+++ Assets/Scripts/Player/HeroThrowingWeapon.cs
@@ -4,38 +4,30 @@ using UnityEngine;
 public class HeroThrowingWeapon : MonoBehaviour
 {
     [SerializeField] private Rigidbody weaponRigidBody;
-    [SerializeField] private float throwSpeed;
-    private Quaternion originalRotation;
-    private Vector3 originalPosition;
+    [SerializeField] private float throwForce;
+    [SerializeField] private float jumpHeight = 2f;
     private void Start()
     {
-        originalRotation = weaponRigidBody.transform.rotation;
-        originalPosition = weaponRigidBody.transform.position;
         StartCoroutine(ThrowWeapon());
     }
 
     private IEnumerator ThrowWeapon()
     {
-        Vector3 throwDirection = new Vector3(0, 0, -0.5f);
-        weaponRigidBody.velocity = throwDirection * throwSpeed;
+        Vector3 jumpForce = new Vector3(0, Mathf.Sqrt(2 * jumpHeight * Mathf.Abs(Physics.gravity.y)), 0);
+
+        weaponRigidBody.AddForce(jumpForce, ForceMode.Impulse);
+
+        yield return new WaitForSeconds(0.5f);
+
+        Vector3 throwDirection = new Vector3(0, 0, -1f);
+
+        weaponRigidBody.AddForce(throwDirection * throwForce, ForceMode.VelocityChange);
+
+        yield return new WaitForSeconds(0.5f);
+
+        weaponRigidBody.constraints = RigidbodyConstraints.FreezeAll;
 
         yield return new WaitForSeconds(5f);
-
-        //float elapsedTime = 0f;
-        //float timerMax = 5f;
-
-        //while (elapsedTime < timerMax)
-        //{
-        //    elapsedTime += Time.deltaTime;
-
-        //    //float progress = elapsedTime / timerMax;
-
-        //    //float angle = 360 * progress;
-
-        //    //weaponRigidBody.rotation = originalRotation * Quaternion.Euler(angle, 0, 0);
-
-        //    yield return null;
-        //}
 
         weaponRigidBody.gameObject.SetActive(false);
     }
